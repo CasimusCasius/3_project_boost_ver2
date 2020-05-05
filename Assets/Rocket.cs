608853cfd0c51af8444fdd38c54f78dc0caf5cd8
CasimusCasius <grzegorz.kazimierczak.sg@gmail.com>
2020-05-05 20:03:36 +0200

    Add movement of rocket, sound of rocket

---
+++ Assets/Rocket.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    AudioSource throttleSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        throttleSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+        ThrottleSFX();
+
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+
+    private void ThrottleSFX()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            throttleSound.Play();
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            throttleSound.Stop();
+        }
+    }
+}
